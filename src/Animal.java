e9304f8ef1236695de10aacbf04f25458a10fb11
mtolubae <noviy.mail@gmail.com>
2025-02-11 16:24:23 +0600

    11.02.2025 commit

---
+++ src/Animal.java
@@ -0,0 +1,95 @@
+public class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void makeSound(){
+        System.out.println("Animal making sound");
+    }
+
+    public void eat(){
+        System.out.println("Animal eating");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+class Dog extends Animal{
+    private String breed;
+    public Dog(String name, int age, String breed){
+        super(name,age);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void makeSound(){
+        System.out.println("Woof Woof");
+    }
+}
+
+class Cat extends Animal{
+    private boolean hasFur;
+
+    public Cat(String name, int age, boolean hasFur) {
+        super(name, age);
+        this.hasFur = hasFur;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public void setHasFur(boolean hasFur) {
+        this.hasFur = hasFur;
+    }
+
+    public void makeSound(){
+        System.out.println("Cries like a baby");
+    }
+
+    public void eat(){
+        System.out.println("Cat is drinking milk");
+    }
+}
+
+class AnimalKingdom{
+    public static void main(String[] args) {
+        Animal animal1 = new Animal("Monkey", 3);
+        Animal animal2 = new Dog("Rex", 1, "Chihuahua");
+        Animal animal3 = new Cat("Murzik", 2, true);
+
+        animal1.makeSound(); animal1.eat();
+        animal2.makeSound(); animal2.eat();
+        animal3.makeSound(); animal3.eat();
+
+    }
+}
+
+
+
+
